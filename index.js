a583fd817005cb8912315d6e71b9c3ef1a871b7f
Rabia Demiroğ <77547046+rabiademirog@users.noreply.github.com>
2023-09-22 01:06:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.random();
+randomNumber1=Math.floor(randomNumber1*6)+1;
+
+var firstImageSrc="images/dice"+randomNumber1+".png";
+document.querySelector("img.img1").setAttribute("src",firstImageSrc);
+
+
+var randomNumber2 = Math.random();
+randomNumber2=Math.floor(randomNumber2*6)+1;
+
+var secondImageSrc="images/dice"+randomNumber2+".png";
+document.querySelector("img.img2").setAttribute("src",secondImageSrc);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+}
+else if(randomNumber1===randomNumber2){
+    document.querySelector("h1").innerHTML="Draw!";
+
+}
