1d5ac90e655f50a7de5bfc0598e02ad4e687a5d7
Rabia Demiroğ <77547046+rabiademirog@users.noreply.github.com>
2023-09-22 15:13:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber1 = Math.floor(Math.random()*6)+1 ;
 
 var firstImageSrc="images/dice"+randomNumber1+".png";
 document.querySelector("img.img1").setAttribute("src",firstImageSrc);
 
 
-var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1 ;
 
 var secondImageSrc="images/dice"+randomNumber2+".png";
 document.querySelector("img.img2").setAttribute("src",secondImageSrc);
